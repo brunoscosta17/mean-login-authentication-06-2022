a795c28dd8179ae78c847e57b0dda8e3ef86fadd
= <=>
2022-06-11 12:53:18 -0300

    setting routes structuring

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { TaskFormComponent } from './modules/task-form/task-form.component';
+import { TaskListComponent } from './modules/task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { RegisterComponent } from './modules/register/register.component';
+import { AuthenticationComponent } from './modules/authentication/authentication.component';
+
+import { AuthGuard } from './shared/guard/auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: TaskListComponent },
+      { path: 'new', component: TaskListComponent },
+      { path: 'edit/:id', component: TaskFormComponent },
+    ], canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
